79a13bc309c9b25d0e08c8791a3bff6618ca1724
Alexey Subbotin <subbotin910@gmail.com>
2018-07-06 20:37:51 +0500

    minor style improvements

--- contracts/AgreementFactory.sol
+++ contracts/AgreementFactory.sol
@@ -13,17 +13,17 @@ contract AgreementFactory is NotarHelpers {
     mapping (uint256 => address) public agreementIdToUser;
     mapping (address => uint256) public agreementsCount;
    
-    function CreateAgreement (address notar, bytes32 data, address[] benefitiars) external existNotar(notar) {
-        uint id = agreements.push(new OneSideAgreement(notar, data, benefitiars)) - 1;
+    function CreateAgreement (address _notar, bytes32 _data, address[] _benefitiars) external existNotar(_notar) {
+        uint id = agreements.push(new OneSideAgreement(_notar, _data, _benefitiars)) - 1;
         agreementIdToUser[id] = msg.sender;
         agreementsCount[msg.sender] = agreementsCount[msg.sender].add(1);
     }
    
-    function GetUserAgreements(address user) public view returns(address[]){
-        address[] memory result = new address[](agreemetnsCount[user]);
+    function GetUserAgreements(address _user) public view returns(address[]){
+        address[] memory result = new address[](agreemetnsCount[_user]);
         uint counter = 0;
         for (uint i = 0; i < agreements.length; i++) {
-            if (agreementIdToUser[i] == user) {
+            if (agreementIdToUser[i] == _user) {
                 result[counter] = agreementIdToUser[i];
             }
         }
--- contracts/OneSideAgreement.sol
+++ contracts/OneSideAgreement.sol
@@ -4,36 +4,36 @@ pragma solidity ^0.4.24;
 
 contract OneSideAgreement {
    
-    address[] _benefitiars;
-    bool public _isCertified;
-    address private _notar;
-    bytes32 private _data;
+    address[] benefitiars;
+    bool public isCertified;
+    address private notar;
+    bytes32 private data;
    
     modifier particularNotar() {
         require(msg.sender == address(_notar));
         _;
     }
 
-    constructor (address notar, bytes32 data, address[] benefitiars) public {
-        _benefitiars = benefitiars;
-        _isCertified = false;
-        _notar = notar;
-        _data = data;
+    constructor (address _notar, bytes32 _data, address[] _benefitiars) public {
+        benefitiars = _benefitiars;
+        isCertified = false;
+        notar = _notar;
+        data = _data;
     }
    
     function GetNotar() view public returns(address) {
-        return _notar;
+        return notar;
     }
 
     function GetData() view public returns(bytes32) {
-        return _data;
+        return data;
     }
 
     function Certify () public particularNotar(){
-        _isCertified = true;
+        isCertified = true;
     }
    
     function UnCertify() public particularNotar(){
-        _isCertified = false;
+        isCertified = false;
     }
 }
