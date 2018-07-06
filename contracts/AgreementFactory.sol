27b11d2e01dd9b1c6ef4e6d57292326d745953c3
Alexey Subbotin <subbotin910@gmail.com>
2018-07-06 19:33:01 +0500

    using safe math for increment

--- contracts/AgreementFactory.sol
+++ contracts/AgreementFactory.sol
@@ -1,8 +1,11 @@
 pragma solidity ^0.4.24;
 
 import "contracts/OneSideAgreement.sol";
+import "./safemath.sol";
 
 contract AgreementFactory {
+
+    using SafeMath for uint256;
    
     OneSideAgreement[] agreements;
    
@@ -10,12 +13,13 @@ contract AgreementFactory {
     
 
     mapping (uint256 => address) public agreementIdToUser;
-    mapping (address => uint256) public agreemetnsCount;
+    mapping (address => uint256) public agreementsCount;
    
     function CreateAgreement (address notar, bytes32 data, address[] benefitiars) external {
         uint id = agreements.push(new OneSideAgreement(notar, data, benefitiars));
         agreementIdToUser[id] = msg.sender;
-        agreemetnsCount[msg.sender]++;
+        //agreemetnsCount[msg.sender]++;
+        agreementsCount[msg.sender] = agreementsCount[msg.sender].add(1);
     }
    
     function GetUserAgreements(address user) public view returns(address[]){
