7a110ec1422bcdd5a29ba303cbc985734ba5958c
Alexey Subbotin <subbotin910@gmail.com>
2018-07-07 14:38:34 +0500

    new funcs for creating and getting agreements

--- contracts/AgreementFactory.sol
+++ contracts/AgreementFactory.sol
@@ -13,8 +13,46 @@ contract AgreementFactory is NotarHelpers {
 
     // An agreement is created (event)
     event AgreementCreated(address _notar, bytes32 _data, address[] _benefitiars);
+    
+    // Mapping with indexes-addresses that stores ids of agreements in the agreements array that belong
+    // to the person with this address
+    mapping (address => uint[]) public addressToId;
 
-    // Mapping with indexes-Id that stores clients 
+    function CreateAgreement (address _notar, bytes32 _data, address[] _benefitiars) external existNotar(_notar) {
+
+        // Creating the agreement
+        OneSideAgreement agreement = new OneSideAgreement(_notar, _data, _benefitiars);
+
+        // Defining the agreement id and adding it to the agreements array
+        uint id = agreements.push(agreement) - 1;
+
+        // Adding the agreement ID to both client's and notary's arrays
+        addressToId[_notar].push(id);
+        addressToId[agreement.GetClient()].push(id);
+
+        emit AgreementCreated(_notar, _data, _benefitiars);
+    }
+
+    function GetContracts(address person) public view returns (OneSideAgreement[]){
+        
+        // All the contracts' ids in the agreement array that belong to the client/notary
+        uint[] storage ids = addressToId[person];
+
+        // Array of that agreements
+        OneSideAgreement[] memory agr = new OneSideAgreement[](ids.length);
+
+        uint counter = 0;
+
+        // Getting those agreements
+        for(uint i = 0; i < ids.length; i++){
+            agr[counter] = agreements[ids[i]];
+            counter = counter.add(1);
+        }
+
+        return agr;
+    }
+
+    /*// Mapping with indexes-Id that stores clients 
     mapping (uint256 => address) public agreementIdToUser;
     // Mapping with indexes-adresses that stores the agreements count
     mapping (address => uint256) public agreementsCount;
@@ -44,5 +82,5 @@ contract AgreementFactory is NotarHelpers {
             }
         }
         return result;
-    }
+    }*/
 }
