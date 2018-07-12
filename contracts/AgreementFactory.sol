1f03df8aeacbc7bc757749c96c47c93348b38716
Alexey Subbotin <subbotin910@gmail.com>
2018-07-12 14:04:57 +0500

    agrfac

--- contracts/AgreementFactory.sol
+++ contracts/AgreementFactory.sol
@@ -4,12 +4,13 @@ import "./OneSideAgreement.sol";
 import "./SafeMath.sol";
 import "./NotarHelpers.sol";
 
-contract AgreementFactory is NotarHelpers {
-
+contract AgreementFactory {
     using SafeMath for uint256;
     
+    NotarHelpers private nh;
+    
     // Array of agreements
-    OneSideAgreement[] agreements;
+    OneSideAgreement[] public agreements;
 
     // An agreement is created (event)
     event AgreementCreated(address _notar, bytes32 _data, address[] _benefitiars);
@@ -17,21 +18,31 @@ contract AgreementFactory is NotarHelpers {
     // Mapping with indexes-addresses that stores ids of agreements in the agreements array that belong
     // to the person with this address
     mapping (address => uint[]) public addressToId;
+    
+    constructor (address _nh) public {
+        nh = NotarHelpers(_nh);
+    }
 
     // Create agreement function (if the notary exists)
-    function CreateAgreement (address _notar, bytes32 _data, address[] _benefitiars) external existNotar(_notar) {
-
-        // Creating the agreement
-        OneSideAgreement agreement = new OneSideAgreement(_notar, _data, _benefitiars);
-
-        // Defining the agreement id and adding it to the agreements array
-        uint id = agreements.push(agreement) - 1;
-
-        // Adding the agreement ID to both client's and notary's arrays
-        addressToId[_notar].push(id);
-        addressToId[agreement.GetClient()].push(id);
-
-        emit AgreementCreated(_notar, _data, _benefitiars);
+    function CreateAgreement (address _notar, bytes32 _data, address[] _benefitiars) external returns(address) {
+        bool flag = nh.GetNotarStatus(_notar);
+        if (flag) {
+            // Creating the agreement
+            OneSideAgreement agreement = new OneSideAgreement(_notar, _data, _benefitiars);
+    
+            // Defining the agreement id and adding it to the agreements array
+            uint id = agreements.push(agreement) - 1;
+    
+            // Adding the agreement ID to both client's and notary's arrays
+            addressToId[_notar].push(id);
+            addressToId[agreement.GetClient()].push(id);
+    
+            emit AgreementCreated(_notar, _data, _benefitiars);
+            
+            return address(agreement);
+        } else {
+            return _notar;
+        }
     }
 
     // Get all the agreements of a person (either client's or notary's)
@@ -53,36 +64,4 @@ contract AgreementFactory is NotarHelpers {
 
         return agr;
     }
-
-    /*// Mapping with indexes-Id that stores clients 
-    mapping (uint256 => address) public agreementIdToUser;
-    // Mapping with indexes-adresses that stores the agreements count
-    mapping (address => uint256) public agreementsCount;
-   
-    // Create agreement function (if the notary exists)
-    function CreateAgreement (address _notar, bytes32 _data, address[] _benefitiars) external existNotar(_notar) {
-        // Defining the agreement id and adding it to the agreements array
-        uint id = agreements.push(new OneSideAgreement(_notar, _data, _benefitiars)) - 1;
-        // Adding the client
-        agreementIdToUser[id] = msg.sender;
-        // Increasing the count
-        agreementsCount[msg.sender] = agreementsCount[msg.sender].add(1);
-
-        emit AgreementCreated(_notar, _data, _benefitiars);
-    }
-    
-    // Get user's agreements function
-    function GetUserAgreements(address _user) public view returns(address[]){
-        // Stores all the agreements
-        address[] memory result = new address[](agreemetnsCount[_user]);
-        
-        uint counter = 0;
-        for (uint i = 0; i < agreements.length; i++) {
-            if (agreementIdToUser[i] == _user) {
-                result[counter] = agreementIdToUser[i];
-                counter = counter.add(1);
-            }
-        }
-        return result;
-    }*/
 }
