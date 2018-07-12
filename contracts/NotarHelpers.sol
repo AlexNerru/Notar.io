224041f47150973bf89e58b2d83780c1f3a95431
Alexey Subbotin <subbotin910@gmail.com>
2018-07-12 14:03:53 +0500

    notarhelpers

--- contracts/NotarHelpers.sol
+++ contracts/NotarHelpers.sol
@@ -18,8 +18,15 @@ contract NotarHelpers is Ownable {
     event NotarAdded(address _notar);
     
     // Mapping with indexes-addresses that stores Notar objects
-    mapping(address => Notar) public  notarAddressToId;
-    
+    mapping(address => Notar) public notarAddressToId;
+
+    // Modifier to check whether a notary exists or not
+    modifier existNotar(address _notarAddress) {
+        if(notarAddressToId[_notarAddress].exist) {
+            _;
+        }
+    }
+
     // Add notary to the mapping function
     function AddNotar (address _notarAddress) onlyOwner public {
         notarAddressToId[_notarAddress] = Notar(_notarAddress, true);
@@ -27,26 +34,21 @@ contract NotarHelpers is Ownable {
     }
     
     // Delete notary from the mapping function (if it exists)
-    function DeleteNotar (address _notarAddress) onlyOwner existNotar(_notarAddress) public {
+    function DeleteNotar (address _notarAddress) onlyOwner public {
         //delete notarAddressToId[_notarAddress];
         notarAddressToId[_notarAddress] = Notar(0, false);
         emit NotarDeleted(_notarAddress);
-    }
+    } 
 
     // Check if msg.sender is a notary
     function AmINotar() public view returns (bool) {
         return notarAddressToId[msg.sender].exist;
     }
 
-    // Checking notary status
+    // Checking notar status
     function GetNotarStatus(address _address) external view returns(bool){
         return notarAddressToId[_address].exist;
     }
-
-    // Modifier to check whether a notary exists or not
-    modifier existNotar(address _notarAddress) {
-        require(notarAddressToId[_notarAddress].exist);
-        _;
-    } 
+ 
 }
  
