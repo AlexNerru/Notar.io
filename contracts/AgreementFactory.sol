8b5968b160e8b9ff7b15b3e191cdf9acfac6c5f6
Alexey Subbotin <subbotin910@gmail.com>
2018-07-12 18:37:49 +0500

    back to OneSi

--- contracts/AgreementFactory.sol
+++ contracts/AgreementFactory.sol
@@ -47,13 +47,13 @@ contract AgreementFactory {
     }
 
     // Get all the agreements of a person (either client's or notary's)
-    function GetAgreements(address person) public view returns (address[]){
+    function GetAgreements(address person) public view returns (OneSideAgreement[]){
         
         // All the agreements' ids in the agreement array that belong to the client/notary
         uint[] storage ids = addressToId[person];
 
         // Array of that agreements
-        address[] memory agr = new address[](ids.length);
+        OneSideAgreement[] memory agr = new OneSideAgreement[](ids.length);
 
         uint counter = 0;
 
